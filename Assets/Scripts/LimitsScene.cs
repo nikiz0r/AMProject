6dcf2e13aaf8ba7fa02a94ca2dd1349c15e47d75
jeanlos3 <jean_clg@hotmail.com>
2016-08-27 17:51:54 -0300

    Atualização

---
+++ Assets/Scripts/LimitsScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitsScene : MonoBehaviour {
+	private Camera cam;
+	private EdgeCollider2D collider;
+	Vector2 downLeft,downRight,upLeft,upRight;
+
+	void Start () {
+		cam = Camera.main;
+		collider = GetComponent<EdgeCollider2D> ();
+
+		downLeft = cam.ScreenToWorldPoint (new Vector3 (0, 0));
+		downRight = cam.ScreenToWorldPoint (new Vector3 (Screen.width, 0));
+		upLeft = cam.ScreenToWorldPoint (new Vector3 (0, Screen.height));
+		upRight = cam.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height));
+
+		collider.points = new Vector2[5]{ downLeft, upLeft, upRight, downRight, downLeft };
+
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,24 +2,48 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-    public float speed, jumpForce, speedBulletL, speedBulletR;
+
+	/* VALORES PARA MOVIMENTACAO PLAYER (PARA EFEITO DE PADRONIZACAO ESSAS VARIAVEIS NAO ESTAO
+	 MAIS PUBLICAS E SIM SEUS VALORES JA INPUTADOS NO START).
+	 * speed: 10
+	 * jumpForce: 600
+	 * speedBullet: 15
+	 */
+
+    private float speed, jumpForce, speedBullet, speedBulletL, speedBulletR, direction, dashForce;
+	private bool grounded, leftSide;
+	private int bulletLimit;
     public Transform groundCheck, gun;
     public LayerMask ground;
-    public GameObject bullet;
-    Rigidbody2D playerRb;
-    Transform playerTr;
-    float direction;
-    bool grounded, leftSide;
+    public GameObject bullet,melee;
+	private Rigidbody2D playerRb;
+	private Transform playerTr;
+
+    
 
     void Start () {
+		speed = 10;
+		jumpForce = 600;
+		dashForce = 5000;
+		speedBullet = 15;
+		speedBulletR = speedBullet;
+		speedBulletL = speedBullet * -1;
+
+		melee.SetActive (false);
         playerRb = GetComponent<Rigidbody2D>();
         playerTr = GetComponent<Transform>();
 	}
 	
 	void Update () {
         grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f,ground);
+
+		//LIMITADOR DE BALAS NA TELA
+		bulletLimit = GameObject.FindGameObjectsWithTag ("Bullet").Length + 1;
+
         Jump();
 		Shoot();
+		Dash ();
+		MeleeAttack ();
 	}
 
     void FixedUpdate(){
@@ -46,14 +70,36 @@ public class Player : MonoBehaviour {
     }
 
     void Shoot(){
-		if (Input.GetButtonDown("Fire1")) {
-			GameObject shoot = (GameObject)Instantiate(bullet, gun.position, transform.rotation);
-            if(leftSide == true){
-                shoot.GetComponent<Rigidbody2D>().velocity = new Vector3(speedBulletL, 0);
-            }
-            else if(leftSide == false){
-                shoot.GetComponent<Rigidbody2D>().velocity = new Vector3(speedBulletR, 0);
-            }
+		if (bulletLimit <= 3) {
+			if (Input.GetButtonDown("Fire1")) {
+				GameObject shoot = (GameObject)Instantiate(bullet, gun.position, transform.rotation);
+				if(leftSide == true){
+					shoot.GetComponent<Rigidbody2D>().velocity = new Vector3(speedBulletL, 0);
+				}
+				else if(leftSide == false){
+					shoot.GetComponent<Rigidbody2D>().velocity = new Vector3(speedBulletR, 0);
+				}
+			}
 		}
     }
+
+	void Dash(){
+		if (Input.GetButtonDown("Fire2")) {
+			if (leftSide == false) {
+				playerRb.AddForce (new Vector2 (dashForce, 0));
+			}
+			else if (leftSide == true) {
+				playerRb.AddForce (new Vector2 (-dashForce, 0));
+			}
+		}
+	}
+
+	void MeleeAttack(){
+		if (Input.GetButtonDown("Fire3")) {
+			melee.SetActive (true);
+		}
+		if (Input.GetButtonUp("Fire3")) {
+			melee.SetActive (false);
+		}
+	}
 }
