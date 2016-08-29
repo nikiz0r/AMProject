7420732d7623a10a3f93a092183c2cb80735d8eb
Rumenigue <niguerag@hotmail.com>
2016-08-28 23:18:01 -0300

    novasFeatures
    Aquabomb adicionado (mecanica), spawn de inimigos (resources/prefabs)

---
+++ Assets/Scripts/BombExplosion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombExplosion : MonoBehaviour {
+
+    private float explosionTime;
+
+	// Use this for initialization
+	void Start () {
+        explosionTime = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(ExplosionLifeTime(explosionTime));
+	}
+
+    IEnumerator ExplosionLifeTime(float timeAlive)
+    {
+        yield return new WaitForSeconds(timeAlive);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+    }
+}
