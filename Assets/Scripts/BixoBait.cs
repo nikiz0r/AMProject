e2a43b620b59d61ae0ff69b2d1a467345c5cb519
Rumenigue Nogueira da Silva <niguerag@hotmail.com>
2016-08-29 22:29:30 -0300

    BixoBait
    Bixo bait feito

---
+++ Assets/Scripts/BixoBait.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BixoBait : MonoBehaviour {
+
+    public GameObject bixao, danger;
+    private MainScript mainScript;
+    private bool bixaoInstanciado = false;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (!bixaoInstanciado)
+            {
+                Instantiate(danger, new Vector2(gameObject.transform.position.x, 4.35f), gameObject.transform.rotation);
+                bixaoInstanciado = true;
+                Instantiate(bixao, new Vector2(gameObject.transform.position.x, 30f), gameObject.transform.rotation);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,10 +5,10 @@ public class Bullet : MonoBehaviour {
 	
 	void OnTriggerEnter2D(Collider2D col){
 		switch (col.transform.tag) {
-		case "enemy1":
+		case "Bomb":
 			Destroy (col.gameObject);
-			break;
+            Destroy(this.gameObject);
+            break;
 		}
-		Destroy (this.gameObject);
 	}
 }
